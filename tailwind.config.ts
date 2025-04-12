37436457226b274339db0e90490b6366c9b2f4f2
Shresth-Deorari <shresthdeorari38@gmail.com>
2025-04-12 13:05:51 +0530

    chore: minor formatting changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: "class",
   content: [
-    './src/components/**/*.{html,js,ts,jsx,tsx}',
-    './src/pages/**/*.{html,js,ts,jsx,tsx}',
-    './index.html',
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+    "./index.html",
   ],
   theme: {
     extend: {
       colors: {
-        brand_yellow: '#FFDF6F',
-        blue: '#6DF7E3',
-        'bg-light': '#F9FAFB',
-        'bg-dark': '#1A202C',
-        'text-light': '#1F2937',
-        'text-dark': '#E5E7EB',
+        brand_yellow: "#FFDF6F",
+        blue: "#6DF7E3",
+        "bg-light": "#F9FAFB",
+        "bg-dark": "#1A202C",
+        "text-light": "#1F2937",
+        "text-dark": "#E5E7EB",
       },
       fontFamily: {
-        sans: ['Open Runde'],
-        serif: ['Catamaran'],
+        sans: ["Open Runde"],
+        serif: ["Catamaran"],
       },
     },
   },
   plugins: [],
-} satisfies Config
+} satisfies Config;
