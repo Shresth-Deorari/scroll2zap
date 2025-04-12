8dd6747b9cb0f0695668c6de12cb9a3b40bfee3c
Shresth-Deorari <shresthdeorari38@gmail.com>
2025-04-12 15:24:52 +0530

    add minimal footer

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,13 @@
 import { LayoutProps } from "../types";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function AppLayout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen p-8 flex flex-col bg-bg-light dark:bg-bg-dark transition:colors duration:300">
       <Header title="Scroll 2 Zap"/>
       <main className="flex-1 px-4 sm:px-6 md:px-8 py-6">{children}</main>
-      {/* Footer */}
+      <Footer/>
     </div>
   );
 }
