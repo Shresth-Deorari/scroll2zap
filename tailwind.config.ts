9be14a5ab7a387f2e9fe7cfcadb8cd182a468c6c
Shresth-Deorari <shresthdeorari38@gmail.com>
2025-04-12 08:25:04 +0530

    chore: configure Tailwind with custom typography, colours and content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,25 @@ import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    './src/components/**/*.{html,js}',
-    './src/pages/**/*.{html,js}',
+    './src/components/**/*.{html,js,ts,jsx,tsx}',
+    './src/pages/**/*.{html,js,ts,jsx,tsx}',
     './index.html',
   ],
   theme: {
-    colors:{},
-    fontFamily:{},
-    extend: {},
+    extend: {
+      colors: {
+        brand_yellow: '#FFDF6F',
+        blue: '#6DF7E3',
+        'bg-light': '#F9FAFB',
+        'bg-dark': '#1A202C',
+        'text-light': '#1F2937',
+        'text-dark': '#E5E7EB',
+      },
+      fontFamily: {
+        sans: ['Open Runde'],
+        serif: ['Catamaran'],
+      },
+    },
   },
   plugins: [],
 } satisfies Config
-
