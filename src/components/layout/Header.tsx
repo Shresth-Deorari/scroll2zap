d5db26fb2c7608e127f0eaff67fe9eddf5e1d126
Shresth-Deorari <shresthdeorari38@gmail.com>
2025-04-25 04:56:53 +0530

    chore: Responsiveness

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,7 +5,7 @@ import ThemeToggle from "../theme/ThemeToggle";
 const Header: FC<HeaderProps> = ({ title = "Scroll 2 Zap" }) => {
   return (
     <header className="bg-[var(--color-header-bg)] text-[var(--color-header-text)]">
-      <div className="mx-60 max-w-7xl flex justify-between items-center px-1 sm:px-2 md:px-3 py-2 text-2xl font-bold font-sans">
+      <div className="mx-4 sm:mx-8 md:mx-16 lg:mx-24 xl:mx-60 max-w-7xl flex justify-between items-center px-2 sm:px-4 md:px-6 py-3 text-2xl font-bold font-sans">
         <span>{title}</span>
         <ThemeToggle />
       </div>
